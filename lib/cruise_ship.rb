6e0ca371e1cb16f46f2445460b445c47f58c0d8a
Jillian Weimer <jilliannweimer@gmail.com>
2018-07-11 03:11:05 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,11 +7,9 @@
 # }
 
 def select_winner(passengers)
-winner = ""
-passengers.each do |suite, name|
+  winner = ""
+  passengers.each do |suite, name|
   if suite == :suite_a && name.start_with?("A")
     winner = name
   end
 end
-
-winner
